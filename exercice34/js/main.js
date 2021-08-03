73eb7b4bcbd293eff6b3f7afc186e6b998477a91
Mohamed-Amir kharmaz <amirpro@hotmail.fr>
2021-08-03 16:12:53 +0200

    correction Exo 34

--- exercice34/js/main.js
+++ exercice34/js/main.js
@@ -1,11 +1,32 @@
-$(function () {
-    $(".btn-success").click(function () {
-        $.ajax({ 
-            url: "https://restcountries.eu/rest/v2/name/usa" + $(".form-contro").val(),
-            success: function (data, statuts, response) {
-                $("#capital").html(data[0].capital);
-                $("#country").html(data[0].name);
-            }
+// $(function () {
+//     $(".btn-success").click(function () {
+//         $.ajax({ 
+//             url: "https://restcountries.eu/rest/v2/name/usa" + $(".form-control").val(),
+//             success: function (data, statuts, response) {
+//                 $("#capital").html(data[0].capital);
+//                 $("#country").html(data[0].name);
+//             }
+//         })
+//     })
+// })
+
+$(
+    function () {
+        $("button.btn.btn-success").click(function () {
+            const countryName = $("#exercise input.form-control.mb-3").val()
+            const url = 'https://restcountries.eu/rest/v2/name/' + countryName
+            $.ajax({
+                url: url,
+                success: function(data, statuts, response) {
+                    
+                    $("#country").html(data[0].name)
+                    $("#capital").html(data[0].capital)
+                    
+                }
+            })
+           
         })
-    })
-})
+        
+    }
+)
+
